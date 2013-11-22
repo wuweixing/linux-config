39f72c34483c8e93135497f6b00a2c9828f2fa40
wuweixing <wwx_2012@live.com>
2013-11-22 10:05:50 +0800

    添加vundle链接

--- config.sh
+++ config.sh
@@ -3,8 +3,9 @@ rm -f ~/.gitconfig
 rm -f ~/.bashrc
 rm -f ~/.bash_profile
 rm -f ~/.vimrc
+rm -rf ~/.vim/bundle/vundle
 ln -s $PWD/.gitconfig ~/.gitconfig
 ln -s $PWD/.bashrc ~/.bashrc
 ln -s ~/.bashrc ~/.bash_profile
 ln -s $PWD/.vimrc ~/.vimrc
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+ln -s $PWD/.vim/bundle/vundle ~/.vim/bundle/vundle
